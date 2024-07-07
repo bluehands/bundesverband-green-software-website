a125e4d9672f98814d23e89f206693865460a8c8
DerSchlnken <163151280+DerSchlnken@users.noreply.github.com>
2024-07-08 01:22:00 +0200

    Update .png to .webp

--- js/mobile.js
+++ js/mobile.js
@@ -9,10 +9,10 @@ window.onload = function(){
 		var a = getNavi.getAttribute('style');
 		if(a){
 			getNavi.removeAttribute('style');
-			document.getElementById('mobile-navigation').style.backgroundImage='url(images/mobile/mobile-menu.png)';
+			document.getElementById('mobile-navigation').style.backgroundImage='url(images/mobile/mobile-menu.webp)';
 		} else {
 			getNavi.style.display='block';
-			document.getElementById('mobile-navigation').style.backgroundImage='url(images/mobile/mobile-close.png)';
+			document.getElementById('mobile-navigation').style.backgroundImage='url(images/mobile/mobile-close.webp)';
 		}
 	};
 	var getElm = getNavi.getElementsByTagName("LI");
@@ -29,11 +29,11 @@ window.onload = function(){
 		var b = sub.getAttribute('style');
 		if(b){
 			sub.removeAttribute('style');
-			getElm[i].lastChild.style.backgroundImage='url(images/mobile/mobile-expand.png)';
+			getElm[i].lastChild.style.backgroundImage='url(images/mobile/mobile-expand.webp)';
 			getElm[i].lastChild.style.backgroundColor='rgba(121, 101, 102, 0.91)';
 		} else {
 			sub.style.display='block';
-			getElm[i].lastChild.style.backgroundImage='url(images/mobile/mobile-collapse.png)';
+			getElm[i].lastChild.style.backgroundImage='url(images/mobile/mobile-collapse.webp)';
 			getElm[i].lastChild.style.backgroundColor='rgba(204, 60, 104, 0.91)';
 		}
 	};
